f87220c2bcaeb3b88d22db4e39c7b5255bf5e0cc
mttbanizi <mttbanizi@gmail.com>
2023-04-12 23:36:36 +0330

    authintication jwt

--- devops/api/urls.py
+++ devops/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include(('devops.blog.urls', 'blog')))
+    path('blog/', include(('devops.blog.urls', 'blog'))),
+    path('users/', include(('devops.users.urls', 'users'))),
+    path('authentication/', include(('devops.authentication.urls', 'authentication'))),
 ]
