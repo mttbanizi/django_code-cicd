f9294c8c37abaa4b61f133addfc36eaba5a795c2
mttbanizi <mttbanizi@gmail.com>
2023-04-14 16:46:08 +0330

    testing

--- devops/api/urls.py
+++ devops/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include(('devops.blog.urls', 'blog'))),
     path('users/', include(('devops.users.urls', 'users'))),
-    path('authentication/', include(('devops.authentication.urls', 'authentication'))),
+    path('auth/', include(('devops.authentication.urls', 'auth'))),
 ]
